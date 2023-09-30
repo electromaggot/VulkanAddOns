7cfec8ceaa9ad6aa95faf534fabee7e15454c363
Tadd Jensen <tadd@megaphonestudios.com>
2023-09-30 10:34:59 -0500

    Add model support, including definition-specifier and loader, which is currently only tiny_obj.

---
+++ AddOns/3DObjects/Model/ModelDefSpec.h
@@ -0,0 +1,32 @@
+//
+// ModelDefSpec.h - 3D Model Definition Specifier
+//	Vulkan Add-Ons
+//
+// Model may be parsed directly from a definition file (for a format
+//	that is supported), created from some indirect definition (like
+//	a truetype font), or generated procedurally.
+//
+// Created 9/15/22 by Tadd Jensen
+//	© 0000 (uncopyrighted; use at will)
+//
+#ifndef ModelDefSpec_h
+#define ModelDefSpec_h
+
+enum SpecType {
+	UNSPECIFIED,
+	FONT_3D,
+	OBJ_FILE_TINY,				// use TinyOBJLoader.h
+	OBJ_FILE_FAST,				// use our own fast OBJ loader
+	OBJ_FILE = OBJ_FILE_TINY	// default to this loader
+};
+
+struct ModelDefSpec {
+	string		filename = "";
+	SpecType	type	 = UNSPECIFIED;
+
+	bool isRequested() {
+		return ! filename.empty() && type != UNSPECIFIED;
+	}
+};
+
+#endif	// ModelDefSpec_h
